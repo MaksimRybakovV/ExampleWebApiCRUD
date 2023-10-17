41d0b4f748fdb1bcf32669be012b283ec338aa84
MaksimRybakovV <mrybakov1406@gmail.com>
2023-10-18 00:19:04 +0500

    Added a customer service interface.

---
+++ Services/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using ExampleWebApiCRUD.Entities.Dtos.Customer;
+using ExampleWebApiCRUD.Entities.Models;
+
+namespace ExampleWebApiCRUD.Services
+{
+    public interface ICustomerService
+    {
+        public Task<ServiceResponce<List<GetCustomerDto>>> GetAllCustomersAsync();
+        public Task<ServiceResponce<GetCustomerDto>> GetCustomerByIdAsync(int id);
+        public Task<ServiceResponce<List<GetCustomerDto>>> AddCustomerAsync(AddCustomerDto newCustomer);
+        public Task<ServiceResponce<GetCustomerDto>> UpdateCustomerAsync(UpdateCustomerDto updatedCustomer);
+        public Task<ServiceResponce<List<GetCustomerDto>>> DeleteCustomerAsync(int id);
+    }
+}
