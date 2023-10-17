7c164f81f4eebf053e31ce7e2e8ea157e28c108c
MaksimRybakovV <mrybakov1406@gmail.com>
2023-10-17 23:30:20 +0500

    Made Dto for the customer's model.

---
+++ Entities/Dtos/Customer/AddCustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using ExampleWebApiCRUD.Entities.Enums;
+
+namespace ExampleWebApiCRUD.Entities.Dtos.Customer
+{
+    public class AddCustomerDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public Genders Gender { get; set; }
+    }
+}
---
+++ Entities/Dtos/Customer/GetCustomerDto.cs
@@ -0,0 +1,15 @@
+﻿using ExampleWebApiCRUD.Entities.Enums;
+
+namespace ExampleWebApiCRUD.Entities.Dtos.Customer
+{
+    public class GetCustomerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public Genders Gender { get; set; }
+    }
+}
