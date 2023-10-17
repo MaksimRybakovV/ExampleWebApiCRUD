d3956af3a0e791f49d81f84ecbb255c4d0f6ae14
MaksimRybakovV <mrybakov1406@gmail.com>
2023-10-17 23:40:53 +0500

    The data context is made.

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using ExampleWebApiCRUD.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleWebApiCRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"DataSource={Environment.CurrentDirectory}/Data/DataBase/ExampleDB.db");
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+    }
+}
