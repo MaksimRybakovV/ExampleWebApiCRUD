4ef64bc1e1dca3fe2a9cf1641079a891c993f6e6
MaksimRybakovV <mrybakov1406@gmail.com>
2023-10-22 18:12:55 +0500

    Added serialization of enum to string.

--- Entities/Enums/Genders.cs
+++ Entities/Enums/Genders.cs
@@ -1,5 +1,8 @@
-﻿namespace ExampleWebApiCRUD.Entities.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace ExampleWebApiCRUD.Entities.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Genders
     {
         Male,
